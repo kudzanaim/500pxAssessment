82a74a64eb68781f5027a181a6aa06e468fcb778
Kudz Murefu <kudzmurefu@gmail.com>
2020-08-26 22:52:10 -0400

    pushed build folder

---
+++ frontend/photoapp/build/precache-manifest.eca9b97fc85887677d98032271a66017.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ce9d9fb8483ff7e27e3960357952aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "955530769ba2949e8c40",
+    "url": "/static/css/main.c2093ef5.chunk.css"
+  },
+  {
+    "revision": "5a6a09d5e001fc2595e5",
+    "url": "/static/js/2.695a6d86.chunk.js"
+  },
+  {
+    "revision": "dacd10a6b1a3b8da5927b29aaaab3785",
+    "url": "/static/js/2.695a6d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955530769ba2949e8c40",
+    "url": "/static/js/main.026a2d01.chunk.js"
+  },
+  {
+    "revision": "c151ec0faac8bef89c4d",
+    "url": "/static/js/runtime-main.2836b84e.js"
+  },
+  {
+    "revision": "a826c7b7526eec88fb314a3c18678536",
+    "url": "/static/media/location.a826c7b7.png"
+  },
+  {
+    "revision": "1d0ce6e17fc87382897fa94f99f269a2",
+    "url": "/static/media/rating.1d0ce6e1.png"
+  }
+]);
